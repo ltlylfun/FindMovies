63b66e6fefb10037f1e898ebfc8d05c42dd75489
Fang ZhiJie <funjee126@qq.com>
2025-02-05 13:23:48 +0800

    重构后端身份验证模块，将控制器和路由迁移到src目录，添加MongoDB连接配置

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRoutes from "./routes/auth.route.js";
-const app = express();
-
-app.use("/api/v1/auth", authRoutes);
-
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
-});
