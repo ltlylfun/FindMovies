b8fc176e1ca817ed0d43bc0e45cfefef4f4ffa1c
Fang ZhiJie <funjee126@qq.com>
2025-02-06 19:07:58 +0800

    添加JWT身份验证和API集成，重构路由

--- backend/src/config/envVars.js
+++ backend/src/config/envVars.js
@@ -5,4 +5,7 @@ dotenv.config();
 export const ENV_VARS = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 5000,
+  JWT_SECRET: process.env.JWT_SECRET,
+  Node_ENV: process.env.NODE_ENV,
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
